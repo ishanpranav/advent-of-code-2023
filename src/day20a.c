3697356ab0241aee7312251e40747c518448ee77
ishanpranav <ishan.pranav@outlook.com>
2024-01-01 16:57:40 -0800

    One last try

--- src/day20a.c
+++ src/day20a.c
@@ -7,344 +7,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define BUFFER_SIZE 32
-#define DELIMITERS ", \n"
-#define EXCEPTION_FORMAT "Error: Format.\n"
-#define EXCEPTION_OUT_OF_MEMORY "Error: Out of memory.\n"
-#define MESSAGE_QUEUE_CAPACITY 64
-#define MODULE_COLLECTION_ENTRIES 89
-#define MODULE_NAME_CAPACITY 12
-#define MODULE_TARGETS_CAPACITY 5
+#define BUFFER_SIZE 1000
 
-struct Message
+union U1
 {
-    char source[MODULE_NAME_CAPACITY];
-    char target[MODULE_NAME_CAPACITY];
     bool pulse;
-};
-
-struct MessageQueue
-{
-    int first;
-    int last;
-    struct Message items[MESSAGE_QUEUE_CAPACITY];
-};
-
-struct PulseEntry
-{
-    struct PulseEntry* nextEntry;
-    char name[MODULE_NAME_CAPACITY];
-    bool pulse;
-};
-
-struct PulseBucket
-{
-    struct PulseEntry* firstEntry;
-    struct PulseBucket* nextBucket;
 };
 
 struct Module
 {
-    struct Module* nextModule;
-    struct PulseBucket* firstBucket;
-    struct PulseBucket buckets[MODULE_COLLECTION_ENTRIES];
-    int targetCount;
-    char name[MODULE_NAME_CAPACITY];
-    char targets[MODULE_TARGETS_CAPACITY][MODULE_NAME_CAPACITY];
-    bool pulse;
+    union U1 impl;
     bool isConjunction;
 };
 
-struct ModuleCollectionEntry
-{
-    struct Module* firstModule;
-    struct ModuleCollectionEntry* nextEntry;
-};
-
-struct ModuleCollection
-{
-    struct ModuleCollectionEntry* firstEntry;
-    struct ModuleCollectionEntry entries[MODULE_COLLECTION_ENTRIES];
-};
-
-typedef char* String;
-typedef struct Message* Message;
-typedef struct MessageQueue* MessageQueue;
-typedef struct PulseEntry* PulseEntry;
-typedef struct PulseBucket* PulseBucket;
-typedef struct Module* Module;
-typedef struct ConjunctionModuleEntry* ConjunctionModuleEntry;
-typedef struct ConjunctionModuleBucket* ConjunctionModuleBucket;
-typedef struct ModuleCollectionEntry* ModuleCollectionEntry;
-typedef struct ModuleCollection* ModuleCollection;
-
-int string_get_hash_code(String instance)
-{
-    int hash = 7;
-
-    for (char* p = instance; *p; p++)
-    {
-        hash = (hash * 31) + *p;
-    }
-
-    return hash;
-}
-
-void message_queue(MessageQueue instance)
-{
-    instance->first = -1;
-    instance->last = -1;
-}
-
-void message_queue_enqueue(MessageQueue instance, Message item)
-{
-    if (instance->first == -1)
-    {
-        instance->first = 0;
-        instance->last = 0;
-    }
-    else if (instance->first && instance->last == MESSAGE_QUEUE_CAPACITY - 1)
-    {
-        instance->last = 0;
-    }
-    else
-    {
-        instance->last++;
-    }
-
-    instance->items[instance->last] = *item;
-}
-
-bool message_queue_try_dequeue(MessageQueue instance, Message result)
-{
-    if (instance->first == -1)
-    {
-        return false;
-    }
-
-    *result = instance->items[instance->first];
-
-    if (instance->first == instance->last)
-    {
-        instance->first = -1;
-        instance->last = -1;
-    }
-    else if (instance->first == MESSAGE_QUEUE_CAPACITY - 1)
-    {
-        instance->first = 0;
-    }
-    else
-    {
-        instance->first++;
-    }
-
-    return true;
-}
-
-void module_add_target(Module instance, String item)
-{
-    strcpy(instance->targets[instance->targetCount], item);
-
-    instance->targetCount++;
-}
-
-bool module_set_pulse(Module instance, String name, bool value)
-{
-    PulseEntry* p;
-    unsigned int hash = string_get_hash_code(name);
-
-    hash %= MODULE_COLLECTION_ENTRIES;
-
-    for (p = &instance->buckets[hash].firstEntry; *p; p = &(*p)->nextEntry)
-    {
-        if (strcmp(name, (*p)->name) == 0)
-        {
-            (*p)->pulse = value;
-
-            return true;
-        }
-    }
-
-    PulseEntry entry = malloc(sizeof * entry);
-
-    if (!entry)
-    {
-        return false;
-    }
-
-    if (!instance->buckets[hash].firstEntry)
-    {
-        PulseBucket first = instance->firstBucket;
-
-        instance->buckets[hash].nextBucket = first;
-        instance->firstBucket = instance->buckets + hash;
-    }
-
-    strcpy(entry->name, name);
-
-    entry->pulse = value;
-    entry->nextEntry = NULL;
-    *p = entry;
-
-    return true;
-}
-
-void module_send(Module instance, bool pulse, MessageQueue queue)
-{
-    for (int i = 0; i < instance->targetCount; i++)
-    {
-        struct Message message;
-
-        message.pulse = pulse;
-
-        strcpy(message.source, instance->name);
-        strcpy(message.target, instance->targets[i]);
-        message_queue_enqueue(queue, &message);
-    }
-}
-
-bool module_respond(Module instance, Message message, MessageQueue queue)
-{
-    if (instance->isConjunction)
-    {
-        if (!module_set_pulse(instance, message->source, message->pulse))
-        {
-            return false;
-        }
-
-        bool result = false;
-
-        for (PulseBucket bucket = instance->buckets;
-            bucket;
-            bucket = bucket->nextBucket)
-        {
-            for (PulseEntry entry = bucket->firstEntry;
-                entry;
-                entry = entry->nextEntry)
-            {
-                if (!entry->pulse)
-                {
-                    result = true;
-
-                    break;
-                }
-            }
-        }
-
-        module_send(instance, result, queue);
-
-        return true;
-    }
-
-    if (!message->pulse)
-    {
-        instance->pulse = !instance->pulse;
-
-        module_send(instance, instance->pulse, queue);
-    }
-
-    return true;
-}
-
-Module module_collection_add_new_module(
-    ModuleCollection instance,
-    bool isConjunction,
-    String name)
-{
-    Module* p;
-    unsigned int hash = string_get_hash_code(name);
-
-    hash %= MODULE_COLLECTION_ENTRIES;
-
-    for (p = &instance->entries[hash].firstModule; *p; p = &(*p)->nextModule)
-    {
-        if (strcmp(name, (*p)->name) == 0)
-        {
-            return NULL;
-        }
-    }
-
-    Module result = malloc(sizeof * result);
-
-    if (!result)
-    {
-        return NULL;
-    }
-
-    if (!instance->entries[hash].firstModule)
-    {
-        ModuleCollectionEntry first = instance->firstEntry;
-
-        instance->entries[hash].nextEntry = first;
-        instance->firstEntry = instance->entries + hash;
-    }
-
-    result->firstBucket = NULL;
-    result->isConjunction = isConjunction;
-    result->nextModule = NULL;
-    result->pulse = false;
-    result->targetCount = 0;
-
-    for (int i = 0; i < MODULE_COLLECTION_ENTRIES; i++)
-    {
-        result->buckets[i].firstEntry = NULL;
-        result->buckets[i].nextBucket = NULL;
-    }
-
-    strcpy(result->name, name);
-
-    *p = result;
-
-    return result;
-}
-
-Module module_collection_find(ModuleCollection instance, String name)
-{
-    unsigned int hash = string_get_hash_code(name);
-
-    hash %= MODULE_COLLECTION_ENTRIES;
-
-    for (Module module = instance->entries[hash].firstModule;
-        module;
-        module = module->nextModule)
-    {
-        if (strcmp(name, module->name) == 0)
-        {
-            return module;
-        }
-    }
-
-    return NULL;
-}
-
-void module_collection_finalize(ModuleCollection instance)
-{
-    for (ModuleCollectionEntry entry = instance->firstEntry;
-        entry;
-        entry = entry->nextEntry)
-    {
-        Module current = entry->firstModule;
-
-        while (current)
-        {
-            Module next = current->nextModule;
-
-            free(current);
-
-            current = next;
-        }
-
-        entry->firstModule = NULL;
-    }
-
-    instance->firstEntry = NULL;
-}
-
 int main()
 {
-    char buffer[BUFFER_SIZE];
     clock_t start = clock();
-    struct ModuleCollection modules = { 0 };
+    char buffer[BUFFER_SIZE];
 
     while (fgets(buffer, sizeof buffer, stdin))
     {
@@ -352,122 +31,23 @@ int main()
 
         if (!mid)
         {
-            fprintf(stderr, EXCEPTION_FORMAT);
+            fprintf(stderr, "Error: Format.\n");
 
             return 1;
         }
 
         *mid = '\0';
 
-        String name;
+        struct Module module;
 
-        if (buffer[0] == '&' || buffer[0] == '%')
-        {
-            name = buffer + 1;
-        }
-        else
-        {
-            name = buffer;
-        }
+        module.isConjunction = buffer[0] == '%';
 
-        Module current = module_collection_add_new_module(
-            &modules,
-            buffer[0] == '%',
-            name);
-
-        if (!current)
-        {
-            fprintf(stderr, EXCEPTION_OUT_OF_MEMORY);
-
-            return 1;
-        }
-
-        for (String target = strtok(mid + 4, DELIMITERS);
-            target;
-            target = strtok(NULL, DELIMITERS))
-        {
-            module_add_target(current, target);
-        }
+        printf("%s -> %s", buffer + 1, mid + 4);
+        module_collection_add(&modules, module);
     }
 
-    for (ModuleCollectionEntry entry = modules.firstEntry;
-        entry;
-        entry = entry->nextEntry)
-    {
-        for (Module module = entry->firstModule;
-            module;
-            module = module->nextModule)
-        {
-            for (int i = 0; i < module->targetCount; i++)
-            {
-                Module target = module_collection_find(
-                    &modules,
-                    module->targets[i]);
-
-                if (!target || !target->isConjunction)
-                {
-                    continue;
-                }
-
-                if (!module_set_pulse(target, module->name, false))
-                {
-                    fprintf(stderr, EXCEPTION_OUT_OF_MEMORY);
-
-                    return 1;
-                }
-            }
-        }
-    }
-
-    int counts[2] = { 0, 1000 };
-    Module broadcaster = module_collection_find(&modules, "broadcaster");
-
-    if (!broadcaster)
-    {
-        fprintf(stderr, "Error: Key not found.\n");
-
-        return 1;
-    }
-
-    for (int i = 0; i < 1; i++)
-    {
-        printf("Iteration %d\n", i);
-
-        struct MessageQueue queue;
-        struct Message currentMessage;
-
-        message_queue(&queue);
-        module_send(broadcaster, false, &queue);
-
-        int k = 0;
-
-        while (message_queue_try_dequeue(&queue, &currentMessage))
-        {
-            printf("%s -> %s : %d\n", currentMessage.source, currentMessage.target, currentMessage.pulse);
-
-            counts[currentMessage.pulse]++;
-
-            Module target = module_collection_find(
-                &modules,
-                currentMessage.target);
-
-            if (!target)
-            {
-                continue;
-            }
-
-            module_respond(target, &currentMessage, &queue);
-
-            if (k > 6) {
-                break;
-            }k++;
-        }
-    }
-
-    long result = counts[0] * counts[1];
-
+    long result = 0;
     printf("20a %ld %lf\n", result, (double)(clock() - start) / CLOCKS_PER_SEC);
-    module_collection_finalize(&modules);
 
     return 0;
 }
